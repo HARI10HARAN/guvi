7d1985c3f090987312563c02cb20d855f9047fd2
HARI10HARAN <47268304+HARI10HARAN@users.noreply.github.com>
2019-02-25 21:52:08 +0530

    Update fattor.py

--- fattor.py
+++ fattor.py
@@ -2,6 +2,6 @@ def factorial(g):
     if g == 0:
         return 1
     else:
-        return g * factorial(n-1)
+        return g * factorial(g-1)
 g=int(input("the fact is "))
 print(factorial(g))
